cc201714f9b92ef73836b27a2512b9b4893cffc4
zainasif767 <zainclasher2002@gmail.com>
2023-01-26 01:42:44 +0500

    Created Variables and added a For loop

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,9 +1,18 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits do
+      suit
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
