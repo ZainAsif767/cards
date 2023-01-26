64acb78ba0fd084e01a84f19749905738381dab6
zainasif767 <zainclasher2002@gmail.com>
2023-01-27 01:03:42 +0500

    Addec two tests cases

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,4 +1,16 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 20
+  end
+
+  test "Shuffling a deck randomizes it" do
+    deck = Cards.create_deck()
+    assert deck != Cards.shuffle(deck)
+    # another way of doing this is
+    # refute deck == Cards.shuffle(deck)
+  end
 end
