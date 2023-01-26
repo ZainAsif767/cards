f2a052bc023b9fa8d4b18a13132416fa7191af45
zainasif767 <zainclasher2002@gmail.com>
2023-01-26 15:15:47 +0500

    func : Deal

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,17 +2,24 @@ defmodule Cards do
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-
-    for suit <- suits do
-      suit
+    # List Comprehension Mapping
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
     end
   end
 
+  # Shuffling our Deck of cards
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  # Checking if the deck contains the card specified
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  # Dealing our Deck by giving the size, the size splits the deck
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
